09c7980c72c8f012952f90efbcd964b6d914d524
yongyeonkim <es-kyy@smiltegate.com>
2022-11-06 13:31:05 +0900

    게시판, 환경설정 추가

--- src/main/java/kyy/springbootkumongcodingproject/SpringbootKumongCodingProjectApplication.java
+++ src/main/java/kyy/springbootkumongcodingproject/SpringbootKumongCodingProjectApplication.java
@@ -1,17 +1,50 @@
 package kyy.springbootkumongcodingproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationContextFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Map;
 
 
 @SpringBootApplication
 @EnableJpaAuditing // AuditionEntityListner 활성화를 위해 추가
-public class SpringbootKumongCodingProjectApplication {
+public class SpringbootKumongCodingProjectApplication implements ApplicationRunner {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootKumongCodingProjectApplication.class, args);
     }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Environment env = context.getEnvironment();
+        System.out.println("env ====> " + env);
+        System.out.println("SPRING_PROFILES_ACTIVE :: " + environment.getProperty("SPRING_PROFILES_ACTIVE"));
+        System.out.println("system env ::: " + System.getenv("SPRING_PROFILES_ACTIVE"));
+
+        //시스템 환경변수 값 전체 가져오기 (key, value 형태)
+//        Map<String, String> map = System.getenv();
+//        for (Map.Entry <String, String> entry: map.entrySet()) {
+//            System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
+//        }
+//
+//        //'TEST_ENV_STRING' 이라는 환경변수의 값 가져오기
+//        String testString = System.getenv("TEST_ENV_STRING");
+
+    }
+
 
 }
--- src/test/java/kyy/springbootkumongcodingproject/repository/board/BoardRepositoryTest.java
+++ src/test/java/kyy/springbootkumongcodingproject/repository/board/BoardRepositoryTest.java
@@ -2,6 +2,7 @@ package kyy.springbootkumongcodingproject.repository.board;
 
 import kyy.springbootkumongcodingproject.entity.auth.Custom;
 import kyy.springbootkumongcodingproject.entity.board.Board;
+import kyy.springbootkumongcodingproject.entity.board.QBoard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -18,44 +21,47 @@ import java.util.stream.IntStream;
 class BoardRepositoryTest {
 
     @Autowired
+    EntityManager em;
+
+    @Autowired
     BoardRepository boardRepository;
 
     /**
      * select
-     *         board0_.bno as bno1_0_0_,
-     *         board0_.mod_date as mod_date2_0_0_,
-     *         board0_.reg_date as reg_date3_0_0_,
-     *         board0_.content as content4_0_0_,
-     *         board0_.title as title5_0_0_,
-     *         board0_.writer_email as writer_e6_0_0_,
-     *         custom1_.email as email1_3_1_,
-     *         custom1_.mod_date as mod_date2_3_1_,
-     *         custom1_.reg_date as reg_date3_3_1_,
-     *         custom1_.name as name4_3_1_,
-     *         custom1_.password as password5_3_1_
-     *     from
-     *         board board0_
-     *     left outer join
-     *         custom custom1_
-     *             on board0_.writer_email=custom1_.email
-     *     where
-     *         board0_.bno=?
+     * board0_.bno as bno1_0_0_,
+     * board0_.mod_date as mod_date2_0_0_,
+     * board0_.reg_date as reg_date3_0_0_,
+     * board0_.content as content4_0_0_,
+     * board0_.title as title5_0_0_,
+     * board0_.writer_email as writer_e6_0_0_,
+     * custom1_.email as email1_3_1_,
+     * custom1_.mod_date as mod_date2_3_1_,
+     * custom1_.reg_date as reg_date3_3_1_,
+     * custom1_.name as name4_3_1_,
+     * custom1_.password as password5_3_1_
+     * from
+     * board board0_
+     * left outer join
+     * custom custom1_
+     * on board0_.writer_email=custom1_.email
+     * where
+     * board0_.bno=?
      */
-//    @Transactional
-    @Test // 즉시로딩
+    @Transactional
+    @Test
     public void testRead1() {
         Optional<Board> result = boardRepository.findById(100L);
+        Board readBoard = result.get();
+        System.out.println(readBoard);
+        System.out.println(readBoard.getWriter());
 
-        Board board = result.get();
 
-        System.out.println(board);
-        System.out.println(board.getWriter());
     }
 
     @Test
     public void insertBoard() {
         IntStream.rangeClosed(1, 100).forEach(i -> {
-            Custom member = Custom.builder().email("admin" + i +"@aaa.com").build();
+            Custom member = Custom.builder().email("admin" + i + "@aaa.com").build();
 
             Board board = Board.builder()
                     .title("Title... " + i)
@@ -69,31 +75,31 @@ class BoardRepositoryTest {
     }
 
     /**
-     *   select
-     *         board0_.bno as bno1_0_0_,
-     *         custom1_.email as email1_3_1_,
-     *         board0_.mod_date as mod_date2_0_0_,
-     *         board0_.reg_date as reg_date3_0_0_,
-     *         board0_.content as content4_0_0_,
-     *         board0_.title as title5_0_0_,
-     *         board0_.writer_email as writer_e6_0_0_,
-     *         custom1_.mod_date as mod_date2_3_1_,
-     *         custom1_.reg_date as reg_date3_3_1_,
-     *         custom1_.name as name4_3_1_,
-     *         custom1_.password as password5_3_1_
-     *     from
-     *         board board0_
-     *     left outer join
-     *         custom custom1_
-     *             on board0_.writer_email=custom1_.email
-     *     where
-     *         board0_.bno=?
+     * select
+     * board0_.bno as bno1_0_0_,
+     * custom1_.email as email1_3_1_,
+     * board0_.mod_date as mod_date2_0_0_,
+     * board0_.reg_date as reg_date3_0_0_,
+     * board0_.content as content4_0_0_,
+     * board0_.title as title5_0_0_,
+     * board0_.writer_email as writer_e6_0_0_,
+     * custom1_.mod_date as mod_date2_3_1_,
+     * custom1_.reg_date as reg_date3_3_1_,
+     * custom1_.name as name4_3_1_,
+     * custom1_.password as password5_3_1_
+     * from
+     * board board0_
+     * left outer join
+     * custom custom1_
+     * on board0_.writer_email=custom1_.email
+     * where
+     * board0_.bno=?
      */
     @Test
     public void testReadWithWriter() {
         Object result = boardRepository.getBoardWithWriter(100L);
 
-        Object[] arr = (Object[])result;
+        Object[] arr = (Object[]) result;
 
         System.out.println(Arrays.toString(arr));
     }
@@ -112,7 +118,7 @@ class BoardRepositoryTest {
     public void testSearchPage() {
         Pageable pageable = PageRequest.of(0, 10,
                 Sort.by("bno").descending()
-                .and(Sort.by("title").ascending()));
+                        .and(Sort.by("title").ascending()));
         Page<Object[]> result = boardRepository.searchPage("tc", "1", pageable);
     }
 
